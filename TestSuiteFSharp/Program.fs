bda3da34a3efb784aa3a39210bf3dff96cf309ab
Jonas Vedsted Sørensen <jonso16@student.sdu.dk>
2020-05-24 15:37:23 +0200

    alternative solution for P2

--- TestSuiteFSharp/Program.fs
+++ TestSuiteFSharp/Program.fs
@@ -14,7 +14,7 @@ open GeneticSharpImplementations.Fitnesses
 open GeneticSharpUtils
 open System.Collections.Generic
 
-type GAResult (GA: GeneticAlgorithm, Termination: int, Seed: int, BestChromosomes: list<IChromosome>) =
+type GAResult (GA: GeneticAlgorithm, Termination: int, Seed: int, BestChromosomes: list<Tuple<int, IChromosome>>) =
     member this.GA = GA
     member this.Termination = Termination
     member this.Seed = Seed
@@ -38,8 +38,9 @@ let CreateGA seed termination : GAResult =
     ga.Termination <- GenerationNumberTermination(termination)
     
     let mutable bestChromosomes = []
-    let handler = EventHandler( fun sender eventArgs -> let g: GeneticAlgorithm =  sender :?> GeneticAlgorithm in  bestChromosomes <- bestChromosomes@[g.BestChromosome])
-    ga.GenerationRan.AddHandler(handler)
+    let handler = EventHandler( fun sender eventArgs -> let g: GeneticAlgorithm =  sender :?> GeneticAlgorithm in  bestChromosomes <- bestChromosomes@[(g.GenerationsNumber, g.BestChromosome)])
+                                                                                                                                                        
+    ga.GenerationRan.AddHandler(handler) 
     ga.Start()
     let res = GAResult(ga, termination, seed, bestChromosomes)
     res
@@ -49,7 +50,7 @@ let CreateGA seed termination : GAResult =
 
 let GAGen : Gen<GAResult> =
     let terminationMin = 1
-    let terminationMax = 100
+    let terminationMax = 1000
     let terminationGen = Gen.choose (terminationMin,terminationMax)
     let seedMin = 0
     let seedMax = Int32.MaxValue
@@ -65,21 +66,6 @@ let TwinGAGen : Gen<GAResult * GAResult> =
     in Gen.map CreateTwin GAGen
 ;;
 
-let CheckIncreasingFitness l =
-    let rec Next (l:list<IChromosome>) (lastGen:IChromosome) =
-        match l with
-        | []  -> true
-        | e::l  -> if (e.Fitness.GetValueOrDefault() - lastGen.Fitness.GetValueOrDefault()) >= 0.
-                    then (Next l e)
-                    else let _ = printf "New should be grater than old. Actual: \n Old best = %f |  New best = %f \n"
-                                        (lastGen.Fitness.GetValueOrDefault())
-                                        (e.Fitness.GetValueOrDefault())in false
-    in
-    match l with
-    | [] -> true
-    | e::[] -> true
-    | e::l -> (Next l e)
-;;
 
 type MyGenerators =
   static member GeneticAlgorithm() =
@@ -90,27 +76,37 @@ type MyGenerators =
       {new Arbitrary<Tuple<GAResult, GAResult>>() with
           override x.Generator = TwinGAGen
           override x.Shrinker t = Seq.empty }
+;;
+
+let rec CheckIncreasingFitness (l1:list<Tuple<int, IChromosome>>) (l2:list<Tuple<int, IChromosome>>)=
+    match (l1, l2) with
+    | ([], []) | (_,[]) | ([],_) -> (0,"",0, "")
+    | (e1::l1, e2::l2) when e1 = e2 -> CheckIncreasingFitness l1 l2
+    | ((i1,c1)::_, (i2,c2)::_) -> (i1, c1.ToString(), i2, c2.ToString())
+;;
 
 // Custom comparison. This compares and print the left and right sides. 
-let (.=.) left right = left = right |@ sprintf "%A = %A" left right
-let (.>=.) left right = left >= right |@ sprintf "%A >= %A" left right
-let (.<=.) left right = left <= right |@ sprintf "%A <= %A" left right
+let (.=.) left right = left = right |@ sprintf "%A = %A" left right;;
+let (.==.) (left:list<Tuple<int, IChromosome>>) (right:list<Tuple<int, IChromosome>>) =
+    let (i1, c1, i2, c2) = CheckIncreasingFitness left right in
+    left = right |@ sprintf "(Index: %i Value: %s) = (Index: %i Value: %s)" i1 c1 i2 c2 
+;;
 
 type GAProperties =
-  static member ``Number of generations match the termination criteria``
+  static member ``P1 - Number of generations match the termination criteria``
     (gaRes:GAResult) =  gaRes.Termination .=. gaRes.GA.GenerationsNumber
-  static member ``Two GA's with the same inputs should result in two solutions with the same fitness``
+  static member ``P2 - N+1 generation has same or better fitness than N``
+    (gaRes:GAResult) = gaRes.BestChromosomes .==. (List.sortBy (fun (i,c) -> c.Fitness.GetValueOrDefault(), i) gaRes.BestChromosomes)
+  static member ``P3 - Two GA's with the same inputs should result in two solutions with the same fitness``
     (g1:GAResult, g2:GAResult) =  g1.BestFitness .=. g2.BestFitness
-  static member ``Two GA's with the same inputs should result in two identical best genes``
+  static member ``P4 - Two GA's with the same inputs should result in two identical best genes``
     (g1:GAResult, g2:GAResult) = g1.BestGenes .=. g2.BestGenes
-  static member ``N+1 generation has same or better fitness than N``
-    (gaRes:GAResult) = CheckIncreasingFitness gaRes.BestChromosomes
 ;;
 
 
 [<EntryPoint>]
 let main argv =
     Arb.register<MyGenerators>()
-    Check.All<GAProperties> ({Config.Quick with MaxTest = 500})
+    Check.All<GAProperties> ({Config.Quick with MaxTest = 10})
     //Check.One({Config.Quick with MaxTest = 1}, GAProperties.``N+1 generation has same or better fitness than N``)
     0 // return an integer exit code
