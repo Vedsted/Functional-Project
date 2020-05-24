c3924484e31b0a04f7357d765189ae7d4fd5a2d0
Jonas Vedsted Sørensen <jonso16@student.sdu.dk>
2020-05-24 09:10:14 +0200

    added return types + clean up + fixed error in list to list

--- TestSuiteFSharp/Program.fs
+++ TestSuiteFSharp/Program.fs
@@ -1,13 +1,11 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-open System
 open FsCheck
 open GeneticSharp.Domain
 open GeneticSharp.Domain.Chromosomes
 open GeneticSharp.Domain.Crossovers
 open GeneticSharp.Domain.Mutations
-open GeneticSharp.Domain.Populations
 open GeneticSharp.Domain.Populations
 open GeneticSharp.Domain.Randomizations
 open GeneticSharp.Domain.Selections
@@ -16,16 +14,16 @@ open GeneticSharpImplementations.Fitnesses
 open GeneticSharpUtils
 open System.Collections.Generic
 
-type GAResult (GA: GeneticAlgorithm, Termination: int, Seed: int, Generations: List<Generation>) =
+type GAResult (GA: GeneticAlgorithm, Termination: int, Seed: int, BestChromosomes: List<IChromosome>) =
     member this.GA = GA
     member this.Termination = Termination
     member this.Seed = Seed
-    member this.Generations = Generations 
+    member this.BestChromosomes = BestChromosomes 
     member this.printInitial () = sprintf "{ Seed: %i, termination: %i}" this.Seed this.Termination
     member this.printResult () = sprintf "{ termination: %i}" this.GA.GenerationsNumber
     override this.ToString () = sprintf "{ HashCode: %i, initial: %s, result: %s }" (this.GetHashCode()) (this.printInitial()) (this.printResult())
 
-let getGA seed termination =
+let CreateGA seed termination : GAResult =
     RandomizationProvider.Current <- FastRandomRandomizationWithSeed()
     FastRandomRandomizationWithSeed.setSeed(seed)
     let selection = EliteSelection()
@@ -37,34 +35,42 @@ let getGA seed termination =
     let ga = GeneticAlgorithm(population, fitness, selection, crossover, mutation)
     ga.Termination <- GenerationNumberTermination(termination)
     
-    let generations = new List<Generation>()
-    let handler = EventHandler( fun sender eventArgs -> let g: GeneticAlgorithm =  sender :?> GeneticAlgorithm in  generations.Add(g.Population.CurrentGeneration))
+    let bestChromosomes = new List<IChromosome>()
+    let handler = EventHandler( fun sender eventArgs -> let g: GeneticAlgorithm =  sender :?> GeneticAlgorithm in  bestChromosomes.Add(g.BestChromosome))
     ga.GenerationRan.AddHandler(handler)
     ga.Start()
-    let res = GAResult(ga, termination, seed, generations)
+    let res = GAResult(ga, termination, seed, bestChromosomes)
     res
 ;;
 
-let IdenticalGARes (gaRes:GAResult) =
-    let gaRes2 = getGA (gaRes.Seed) (gaRes.Termination)
-    (gaRes, gaRes2)
 
-let GAGen =
-    let seedIntervalGen = Gen.choose (0,Int32.MaxValue) in
-    let terminationIntervalGen = Gen.choose (1,100) in 
-    Gen.map2 getGA seedIntervalGen terminationIntervalGen;;
 
-let GAClones = Gen.map IdenticalGARes GAGen
+let GAGen : Gen<GAResult> =
+    let terminationMin = 1
+    let terminationMax = 100
+    let terminationGen = Gen.choose (terminationMin,terminationMax)
+    let seedMin = 0
+    let seedMax = Int32.MaxValue
+    let seedGen = Gen.choose (seedMin,seedMax)
+    Gen.map2 CreateGA seedGen terminationGen
+;;
+
+let GAClones : Gen<GAResult * GAResult> =
+    let IdenticalGARes (gaRes:GAResult) =
+        let gaRes2 = CreateGA (gaRes.Seed) (gaRes.Termination) in
+        (gaRes, gaRes2)
+    in Gen.map IdenticalGARes GAGen
+;;
 
 let CheckIncreasingFitness l =
-    let rec Next (l:list<Generation>) (lastGen:Generation) =
+    let rec Next (l:list<IChromosome>) (lastGen:IChromosome) =
         match l with
         | []  -> true
-        | e::l  -> if (e.BestChromosome.Fitness.GetValueOrDefault() - lastGen.BestChromosome.Fitness.GetValueOrDefault()) >= 0.
+        | e::l  -> if (e.Fitness.GetValueOrDefault() - lastGen.Fitness.GetValueOrDefault()) >= 0.
                     then (Next l e)
-                    else let _ = printf "New should be grater than old. Actual: \n Old best = %f  -  New best = %f \n"
-                                        (lastGen.BestChromosome.Fitness.GetValueOrDefault())
-                                        (e.BestChromosome.Fitness.GetValueOrDefault()) in false
+                    else let _ = printf "New should be grater than old. Actual: \n Old best = %f |  New best = %f \n"
+                                        (lastGen.Fitness.GetValueOrDefault())
+                                        (e.Fitness.GetValueOrDefault())in false
     in
     match l with
     | [] -> true
@@ -72,10 +78,13 @@ let CheckIncreasingFitness l =
     | e::l -> (Next l e)
 ;;
 
-let rec listtolist (l:List<Generation>) (acc:list<Generation>)=
+(*
+ * List to list is created to convert mutable List to immutable list 
+ *)
+let rec ListToList (l:List<IChromosome>) (acc:list<IChromosome>)=
     match l.Count with
     | 0 -> acc
-    | i -> let x = l.[0] in let _ =  l.RemoveAt(0) in listtolist l (x::acc)
+    | i -> let x = l.[0] in let _ =  l.RemoveAt(0) in ListToList l (acc@[x])
 ;;
 
 type MyGenerators =
@@ -99,13 +108,13 @@ type GAProperties =
   static member ``Number of generations match the termination criteria`` (gaRes:GAResult) =  gaRes.Termination .=. gaRes.GA.GenerationsNumber
   static member ``Two GA's with the same inputs should result in two solutions with the same fitness`` (g1:GAResult, g2:GAResult) =  g1.GA.BestChromosome.Fitness.GetValueOrDefault() .=. g2.GA.BestChromosome.Fitness.GetValueOrDefault()
   static member ``Two GA's with the same inputs should result in two identical best genes`` (g1:GAResult, g2:GAResult) =  g1.GA.BestChromosome.GetGenes() .=. g2.GA.BestChromosome.GetGenes()
-  static member ``N+1 generation has same or better fitness than N`` (gaRes:GAResult) = let l = listtolist (gaRes.Generations) [] in CheckIncreasingFitness l
+  static member ``N+1 generation has same or better fitness than N`` (gaRes:GAResult) = let l = ListToList (gaRes.BestChromosomes) [] in CheckIncreasingFitness l
 ;;
 
 
 [<EntryPoint>]
 let main argv =
     Arb.register<MyGenerators>()
-    //Check.All<GAProperties> ({Config.Quick with MaxTest = 500})
-    Check.One({Config.Quick with MaxTest = 1}, GAProperties.``N+1 generation has same or better fitness than N``)
+    Check.All<GAProperties> ({Config.Quick with MaxTest = 500})
+    //Check.One({Config.Quick with MaxTest = 1}, GAProperties.``N+1 generation has same or better fitness than N``)
     0 // return an integer exit code
